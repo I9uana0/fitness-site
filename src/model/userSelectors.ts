9d049cce7f2c6b5429891f3074df48f84ec398e6
I9uana0 <ivchi06@gmail.com>
2026-06-03 19:40:02 +0300

    fix: deleted unused file

--- src/model/userSelectors.ts
+++
@@ -1,5 +0,0 @@
-import { AuthStore } from "./useUserState";
-
-export const selectIsAuth = (state: AuthStore) => state.isAuth;
-export const selectLogin = (state: AuthStore) => state.login;
-export const selectLogout = (state: AuthStore) => state.logout;
